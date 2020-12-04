3f5d0a48e120518e382025fa18f12d078f6e6f60
dxy2233 <691369338@qq.com>
2020-12-04 17:44:26 +0800

    save

--- main.go
+++ main.go
@@ -7,6 +7,10 @@ import (
 	"github.com/360EntSecGroup-Skylar/excelize/v2"
 )
 
+var (
+	limit = 1130000
+)
+
 func main() {
 	f, err := excelize.OpenFile("base.xlsx")
 	if err != nil {
@@ -14,54 +18,63 @@ func main() {
 		return
 	}
 
-	newF := excelize.NewFile()
-	index := newF.NewSheet("模板")
-	newF.SetActiveSheet(index)
-
+	baseSlice := [][]string{}
 	rows, _ := f.GetRows("蓝票")
-	// for rowIndex, row := range rows {
-	// 	for colIndex, colCell := range row {
-	// 		if colIndex > 0 {
-	// 			break
-	// 		}
-	// 		for _, r := range colCell {
-	// 			if unicode.IsNumber(r) {
-	// 				newF.SetSheetRow("模板", "A"+strconv.Itoa(rowIndex), &row)
-	// 			}
-	// 		}
-	// 	}
-	// }
-
+	customerName := ""
 	for i := 0; i < len(rows); i++ {
 		row := rows[i]
-		fmt.Println(row)
 		if len(row) > 0 {
+			if row[0] == "客户名称:" {
+				customerName = row[1]
+			}
 			isNum := false
 			for _, r := range row[0] {
 				if unicode.IsNumber(r) {
 					isNum = true
 				}
 			}
-			if !isNum {
-				f.RemoveRow("蓝票", i+1)
-				rows, _ = f.GetRows("蓝票")
-				i--
+			if isNum && row[6] != "" && row[6] != "0" {
+				row = append(row[:2], append([]string{customerName}, row[2:]...)...)
+				baseSlice = append(baseSlice, row)
 			}
-		} else {
-			f.RemoveRow("蓝票", i+1)
-			rows, _ = f.GetRows("蓝票")
-			i--
 		}
 	}
-
-	// f.RemoveRow("蓝票", 1)
-	// f.DuplicateRowTo("蓝票", 1, 2)
-	// rows, _ = f.GetRows("蓝票")
-	// for _, row := range rows {
-	// 	fmt.Println(row[0])
-	// }
-
-	if err := f.SaveAs("Book1.xlsx"); err != nil {
-		fmt.Println(err)
+	for _, v := range baseSlice {
+		fmt.Println(v)
 	}
+
+	// rows, _ := f.GetRows("蓝票")
+	// customerName := ""
+	// for i := 0; i < len(rows); i++ {
+	// 	row := rows[i]
+	// 	if len(row) > 0 {
+	// 		if row[0] == "客户名称:" {
+	// 			customerName = row[1]
+	// 		}
+	// 		isNum := false
+	// 		for _, r := range row[0] {
+	// 			if unicode.IsNumber(r) {
+	// 				isNum = true
+	// 			}
+	// 		}
+	// 		if !isNum {
+	// 			f.RemoveRow("蓝票", i+1)
+	// 			rows, _ = f.GetRows("蓝票")
+	// 			i--
+	// 		} else {
+	// 			f.SetCellValue("蓝票", "H"+strconv.Itoa(i+1), customerName)
+	// 		}
+	// 	} else {
+	// 		f.RemoveRow("蓝票", i+1)
+	// 		rows, _ = f.GetRows("蓝票")
+	// 		i--
+	// 	}
+	// }
+	// f.RemoveRow("蓝票", 1)
+	// f.RemoveCol("蓝票", "I")
+	// f.RemoveCol("蓝票", "J")
+
+	// if err := f.SaveAs("Book1.xlsx"); err != nil {
+	// 	fmt.Println(err)
+	// }
 }
