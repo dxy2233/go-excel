deb09d9610b91a31630a3d3f69c0df677de4cedf
dxy2233 <691369338@qq.com>
2020-12-06 23:08:15 +0800

    save

--- main.go
+++ main.go
@@ -2,6 +2,7 @@ package main
 
 import (
 	"fmt"
+	"strconv"
 	"unicode"
 
 	"github.com/360EntSecGroup-Skylar/excelize/v2"
@@ -18,14 +19,18 @@ func main() {
 		return
 	}
 
-	baseSlice := [][]string{}
+	// 过滤数据，添加公司名, 按公司名分组
+	baseSlice := [][][]string{}
 	rows, _ := f.GetRows("蓝票")
 	customerName := ""
+	customerIndex := -1
 	for i := 0; i < len(rows); i++ {
 		row := rows[i]
 		if len(row) > 0 {
 			if row[0] == "客户名称:" {
 				customerName = row[1]
+				customerIndex++
+				baseSlice = append(baseSlice, [][]string{})
 			}
 			isNum := false
 			for _, r := range row[0] {
@@ -35,44 +40,51 @@ func main() {
 			}
 			if isNum && row[6] != "" && row[6] != "0" {
 				row = append(row[:2], append([]string{customerName}, row[2:]...)...)
-				baseSlice = append(baseSlice, row)
+				baseSlice[customerIndex] = append(baseSlice[customerIndex], row)
 			}
 		}
 	}
-	for _, v := range baseSlice {
-		fmt.Println(v)
+
+	// 大于limit的对半分
+	for _, items := range baseSlice {
+		for index, item := range items {
+			itemPrice, _ := strconv.Atoi(item[7])
+			if itemPrice > limit {
+				itemNumber, _ := strconv.Atoi(item[5])
+				splitNumber := itemNumber / 2
+				fmt.Println(splitNumber)
+				items = append(items[:index+1], append([][]string{item}, items[index+1:]...)...)
+			}
+		}
 	}
 
-	// rows, _ := f.GetRows("蓝票")
-	// customerName := ""
-	// for i := 0; i < len(rows); i++ {
-	// 	row := rows[i]
-	// 	if len(row) > 0 {
-	// 		if row[0] == "客户名称:" {
-	// 			customerName = row[1]
-	// 		}
-	// 		isNum := false
-	// 		for _, r := range row[0] {
-	// 			if unicode.IsNumber(r) {
-	// 				isNum = true
-	// 			}
-	// 		}
-	// 		if !isNum {
-	// 			f.RemoveRow("蓝票", i+1)
-	// 			rows, _ = f.GetRows("蓝票")
-	// 			i--
+	// 添加群组号
+	// ticketNum := 1
+	// for _, items := range baseSlice {
+	// 	accrued := 0
+	// 	for _, item := range items {
+	// 		itemPrice, _ := strconv.Atoi(item[7])
+	// 		// fmt.Println(itemPrice + accrued)
+	// 		if itemPrice+accrued < limit {
+	// 			accrued = itemPrice + accrued
+	// 			item[0] = strconv.Itoa(ticketNum)
 	// 		} else {
-	// 			f.SetCellValue("蓝票", "H"+strconv.Itoa(i+1), customerName)
+	// 			ticketNum++
+	// 			item[0] = strconv.Itoa(ticketNum)
+	// 			accrued = itemPrice
 	// 		}
-	// 	} else {
-	// 		f.RemoveRow("蓝票", i+1)
-	// 		rows, _ = f.GetRows("蓝票")
-	// 		i--
+	// 		fmt.Println(item)
 	// 	}
+	// 	fmt.Println("")
+	// 	ticketNum++
 	// }
-	// f.RemoveRow("蓝票", 1)
-	// f.RemoveCol("蓝票", "I")
-	// f.RemoveCol("蓝票", "J")
+
+	for _, items := range baseSlice {
+		for _, item := range items {
+			fmt.Println(item)
+		}
+		fmt.Println("")
+	}
 
 	// if err := f.SaveAs("Book1.xlsx"); err != nil {
 	// 	fmt.Println(err)
