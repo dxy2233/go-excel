d26c10be37cf971e723ecf262a453373229b3c95
dxy2233 <691369338@qq.com>
2020-12-09 17:53:14 +0800

    测试云函数f

--- go.mod
+++ go.mod
@@ -6,4 +6,5 @@ require (
 	github.com/360EntSecGroup-Skylar/excelize v1.4.1
 	github.com/360EntSecGroup-Skylar/excelize/v2 v2.3.1
 	github.com/gorilla/mux v1.8.0
+	github.com/tencentyun/scf-go-lib v0.0.0-20200624065115-ba679e2ec9c9
 )
--- main.go
+++ main.go
@@ -1,12 +1,27 @@
 package main
 
 import (
-	"net/http"
+	"context"
+	"fmt"
 
-	"invoice/router"
+	"github.com/tencentyun/scf-go-lib/cloudfunction"
 )
 
+// DefineEvent test
+type DefineEvent struct {
+	Key1 string `json:"key1"`
+	Key2 string `json:"key2"`
+}
+
+func hello(ctx context.Context, event DefineEvent) (string, error) {
+	fmt.Println("key1:", event.Key1)
+	fmt.Println("key2:", event.Key2)
+	return "hello", nil
+}
+
 func main() {
-	r := router.Lent()
-	http.ListenAndServe(":8999", r)
+	cloudfunction.Start(hello)
+
+	// r := router.Lent()
+	// http.ListenAndServe(":80", r)
 }
